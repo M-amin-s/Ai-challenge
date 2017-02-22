0300fcdf5c41cc7d8327893d138b9c7a9ea3b20b
reza <ab.reza@live.com>
2017-02-22 14:53:47 +0330

    Add files via upload

---
+++ AIC17-Client-Java-2.2/src/client/AI.java
@@ -0,0 +1,128 @@
+package client;
+
+import client.model.*;
+
+import java.util.Random;
+
+/**
+ * AI class.
+ * You should fill body of the method {@link #doTurn}.
+ * Do not change name or modifiers of the methods or fields
+ * and do not add constructor for this class.
+ * You can add as many methods or fields as you want!
+ * Use world parameter to access and modify game's
+ * world!
+ * See World interface for more details.
+ */
+public class AI {
+
+    public void doTurn(World game) {
+
+        Random rand = new Random();
+        BeetleType defender,attacker;
+        Cell[][] cells = game.getMap().getCells();
+        defender = findDefender(cells);
+        if (defender == BeetleType.HIGH)
+            attacker = BeetleType.LOW;
+        else
+            attacker = BeetleType.HIGH;
+        if (game.getCurrentTurn() == 0) {
+            AttackerStrategy(game, attacker);
+            DefenderStrategy(game, defender);
+        } else if(game.getCurrentTurn() % 10 == 4 && game.getCurrentTurn() > 13) {
+            float avg = avgPowerOfEnemy(cells);
+            for (int i=0;i<cells.length;i++){
+                for (int j=0;j<cells[i].length;j++) {
+                    if (cells[i][j].getBeetleEntity() != null &&  (((Beetle)cells[i][j].getBeetleEntity()).getCellState() == CellState.Ally)){
+                        if(((Beetle)cells[i][j].getBeetleEntity()).getPower() > avg && ((Beetle)cells[i][j].getBeetleEntity()).getBeetleType() == BeetleType.LOW){
+                            game.changeType(((Beetle)cells[i][j].getBeetleEntity()), BeetleType.HIGH);
+                            System.out.println(i + " " + j);
+                        }
+                    }
+                }
+            }
+            for (int i=0;i<cells.length;i++){
+                for (int j=0;j<cells[i].length;j++) {
+                    if (cells[i][j].getBeetleEntity() != null &&  (((Beetle)cells[i][j].getBeetleEntity()).getCellState() == CellState.Ally)){
+                        if(((Beetle)cells[i][j].getBeetleEntity()).getPower() < avg && ((Beetle)cells[i][j].getBeetleEntity()).getBeetleType() == BeetleType.HIGH){
+                            game.changeType(((Beetle)cells[i][j].getBeetleEntity()), BeetleType.LOW);
+                            System.out.println(i + " " + j);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    public BeetleType findDefender(Cell[][] cells){
+        int hasWingLow=0,hasWingHigh=0;
+        for (int i=0;i<cells.length;i++){
+            for (int j=0;j<cells[i].length;j++) {
+                if (cells[i][j].getBeetleEntity() != null &&((Beetle)cells[i][j].getBeetleEntity()).has_winge() &&  (((Beetle)cells[i][j].getBeetleEntity()).getCellState() == CellState.Ally)){
+                    if (((Beetle) cells[i][j].getBeetleEntity()).getBeetleType() == BeetleType.LOW){
+                        hasWingLow++;
+                    }
+                    else
+                        hasWingHigh++;
+                }
+            }
+        }
+        if (hasWingHigh >= hasWingLow)
+            return BeetleType.HIGH;
+        else
+            return BeetleType.LOW;
+    }
+
+    public void AttackerStrategy(World game, BeetleType attacker) {
+        for (int i=0;i<3;i++){
+            for (int j=0;j<2;j++){
+                for (int k=0;k<3;k++){
+                    if ((i==2 && k==0) || (i==2 && k==2 && j==0))
+                        game.changeStrategy(attacker, CellState.values()[i],
+                                CellState.values()[j], CellState.values()[k], Move.turnRight);
+                    else
+                        game.changeStrategy(attacker, CellState.values()[i],
+                                CellState.values()[j], CellState.values()[k], Move.stepForward);
+                }
+            }
+        }
+    }
+
+    public void DefenderStrategy(World game, BeetleType defender){
+        for (int i=0;i<2;i++){
+            game.changeStrategy(defender, CellState.Ally,
+                    CellState.values()[i], CellState.Ally, Move.stepForward);
+            game.changeStrategy(defender, CellState.Ally,
+                    CellState.values()[i], CellState.Enemy, Move.turnRight);
+            game.changeStrategy(defender, CellState.Ally,
+                    CellState.values()[i], CellState.Blank, Move.turnLeft);
+            game.changeStrategy(defender, CellState.Enemy,
+                    CellState.values()[i], CellState.Ally, Move.turnRight);
+            game.changeStrategy(defender, CellState.Enemy,
+                    CellState.values()[i], CellState.Enemy, Move.turnLeft);
+            game.changeStrategy(defender, CellState.Enemy,
+                    CellState.values()[i], CellState.Blank, Move.turnLeft);
+            game.changeStrategy(defender, CellState.Blank,
+                    CellState.values()[i], CellState.Ally, Move.stepForward);
+            game.changeStrategy(defender, CellState.Blank,
+                    CellState.values()[i], CellState.Enemy, Move.turnRight);
+            game.changeStrategy(defender, CellState.Blank,
+                    CellState.values()[i], CellState.Blank, Move.stepForward);
+        }
+    }
+    public static float avgPowerOfEnemy(Cell[][] cells){
+        float sum = 0;
+        int counter = 0;
+        for (int i=0;i<cells.length;i++){
+            for (int j=0;j<cells[i].length;j++) {
+                if (cells[i][j].getBeetleEntity() != null &&  (((Beetle)cells[i][j].getBeetleEntity()).getCellState() == CellState.Enemy)){
+                    counter++;
+                    sum += ((Beetle)cells[i][j].getBeetleEntity()).getPower();
+                }
+            }
+        }
+        return sum/counter;
+    }
+
+}
